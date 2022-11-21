1e8820f48d3a1b226de5efe5b2d28b772deca92d
Nguyễn Hải Phong <87389297+DevPhongPhong@users.noreply.github.com>
2022-11-21 18:42:42 +0700

    fix cant join and paing search

--- btl/src/main/java/com/group5/btl/dto/swap/JoinSwapCreate.java
+++ btl/src/main/java/com/group5/btl/dto/swap/JoinSwapCreate.java
@@ -2,9 +2,11 @@ package com.group5.btl.dto.swap;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class JoinSwapCreate {
 	
 	private int userId;
