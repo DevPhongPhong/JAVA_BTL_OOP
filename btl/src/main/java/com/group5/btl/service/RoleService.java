f0a2f5e1d52212b642a80143e275331f1c914eb5
Nguyễn Hải Phong <87389297+DevPhongPhong@users.noreply.github.com>
2022-11-21 17:50:23 +0700

    delete all

--- btl/src/main/java/com/group5/btl/service/RoleService.java
+++
@@ -1,7 +0,0 @@
-package com.group5.btl.service;
-
-import com.group5.btl.model.Role;
-
-public interface RoleService {
-	Role findByName(String name);
-}
--- btl/src/main/java/com/group5/btl/service/sector/SectorService.java
+++
@@ -1,7 +0,0 @@
-package com.group5.btl.service.sector;
-
-import com.group5.btl.model.Sector;
-
-public interface SectorService {
-	Sector findByCode(String code);
-}
